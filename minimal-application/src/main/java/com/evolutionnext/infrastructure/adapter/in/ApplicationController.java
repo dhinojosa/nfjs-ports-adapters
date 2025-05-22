a8c7ffbe0eba2ccb106c63e96d442d7576d13bdd
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2025-05-22 18:52:01 +0100

    Added better comments in the `Controller`

--- minimal-application/src/main/java/com/evolutionnext/infrastructure/adapter/in/ApplicationController.java
+++ minimal-application/src/main/java/com/evolutionnext/infrastructure/adapter/in/ApplicationController.java
@@ -9,10 +9,22 @@ import com.evolutionnext.application.port.in.ForCalculatingTaxes;
 public class ApplicationController {
     private final ForCalculatingTaxes forCalculatingTaxes;
 
+    /**
+     * Inject the driving port (interface), it is up to the application
+     * developer to wire whatever driving implementation that they wish
+     *
+     * @param forCalculatingTaxes the driving port
+     */
     public ApplicationController(ForCalculatingTaxes forCalculatingTaxes) {
         this.forCalculatingTaxes = forCalculatingTaxes;
     }
 
+    /**
+     * I decided here to mimic create a DTO by returning a formatted string
+     *
+     * @param amount the amount that is to be taxed
+     * @return the string representation of the tax applied
+     */
     public String getTaxCalculationStringFor(double amount) {
         double result = forCalculatingTaxes.taxOn(amount);
         return String.format("Tax calculated for the amount $%.2f will be $%.2f", amount, result);
