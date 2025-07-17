196c39d4684e30bf87c81cedb08edd556ea38e1e
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2025-07-17 06:45:18 -0600

    Fix formatting in `ClientOrderCommand` interface

--- full-application/src/main/java/com/evolutionnext/application/commands/order/ClientOrderCommand.java
+++ full-application/src/main/java/com/evolutionnext/application/commands/order/ClientOrderCommand.java
@@ -1,5 +1,6 @@
 package com.evolutionnext.application.commands.order;
 
 
-public sealed interface ClientOrderCommand extends OrderCommand permits CancelOrder, AddOrderItem, InitializeOrder, SubmitOrder {}
+public sealed interface ClientOrderCommand extends OrderCommand
+    permits CancelOrder, AddOrderItem, InitializeOrder, SubmitOrder {}
 
