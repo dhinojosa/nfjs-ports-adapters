5fcedfc981e595b2b01274f415acad05ff4d41ce
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2026-05-27 17:11:07 -0500

    Remove `CustomerCreated` record from the results package

--- full-application/src/main/java/com/evolutionnext/application/results/customer/command/CustomerCreated.java
+++
@@ -1,7 +0,0 @@
-package com.evolutionnext.application.results.customer.command;
-
-
-import com.evolutionnext.domain.aggregates.customer.CustomerId;
-
-public record CustomerCreated(CustomerId customerId) implements CustomerResult {
-}
